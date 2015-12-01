56fa504c682312c4ff60b66bc9db0ae14b6a1cb8
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-12-01 02:39:27 -0600

    Commit of UpdatedInterface v1.4

--- src/DBCoordinator.java
+++ src/DBCoordinator.java
@@ -9,18 +9,19 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DBCoordinator2 {
+public class DBCoordinator {
 	private Connection con;
 	private Statement stmt;
 	
 	// The query results will be store in List<ArrayList<Object>>, you need to do the type casting in your code
 	public List<ArrayList<Object>> queryData(String sqlCmd) {
 		List<ArrayList<Object>> res = new ArrayList<ArrayList<Object>>();
+		String originStr = sqlCmd;
 		sqlCmd = sqlCmd.toUpperCase();
 		try {
 			if((sqlCmd.matches(".*\\sINSERT\\s.*") || sqlCmd.matches("INSERT\\s.*") || 
 			   sqlCmd.matches(".*\\sUPDATE\\s.*") || sqlCmd.matches("UPDATE\\s.*") ||
-			   sqlCmd.matches(".*\\sDELETE\\s.*") || sqlCmd.matches("DELETE\\s.*")) == false) 
+			   sqlCmd.matches(".*\\sDELETE\\s.*") || sqlCmd.matches("DELETE\\s.*")) == true) 
 				throw new IllegalArgumentException("SQL contains non select command, such as Insert, Update, Delete.");
 			
 			Class.forName("org.sqlite.JDBC");
@@ -28,6 +29,7 @@ public class DBCoordinator2 {
 			con.setAutoCommit(false);
 			System.out.println("Opened database successfully");
 
+			sqlCmd = originStr;
 			stmt = con.createStatement();	
 			ResultSet rs = stmt.executeQuery(sqlCmd);
 			while(rs.next()) {
@@ -35,6 +37,7 @@ public class DBCoordinator2 {
 				for(int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
 					tmpRes.add(rs.getObject(i + 1));
 				}
+				res.add(tmpRes);
 			}
 			rs.close();
 			stmt.close();
@@ -55,7 +58,7 @@ public class DBCoordinator2 {
 		try {
 			if((sqlCmd.matches(".*\\sINSERT\\s.*") || sqlCmd.matches("INSERT\\s.*") || 
 					   sqlCmd.matches(".*\\sUPDATE\\s.*") || sqlCmd.matches("UPDATE\\s.*") ||
-					   sqlCmd.matches(".*\\sSELECT\\s.*") || sqlCmd.matches("SELECT\\s.*")) == false) 
+					   sqlCmd.matches(".*\\sSELECT\\s.*") || sqlCmd.matches("SELECT\\s.*")) == true) 
 						throw new IllegalArgumentException("SQL contains non select command, such as Insert, Update, Select.");
 			
 			Class.forName("org.sqlite.JDBC");
@@ -95,7 +98,7 @@ public class DBCoordinator2 {
 		try {
 			if((sqlCmd.matches(".*\\sSELECT\\s.*") || sqlCmd.matches("SELECT\\s.*") || 
 					   sqlCmd.matches(".*\\sUPDATE\\s.*") || sqlCmd.matches("UPDATE\\s.*") ||
-					   sqlCmd.matches(".*\\sDELETE\\s.*") || sqlCmd.matches("DELETE\\s.*")) == false) 
+					   sqlCmd.matches(".*\\sDELETE\\s.*") || sqlCmd.matches("DELETE\\s.*")) == true) 
 						throw new IllegalArgumentException("SQL contains non select command, such as Select, Update, Delete.");
 			
 			Class.forName("org.sqlite.JDBC");
@@ -135,7 +138,7 @@ public class DBCoordinator2 {
 		try {
 			if((sqlCmd.matches(".*\\sINSERT\\s.*") || sqlCmd.matches("INSERT\\s.*") || 
 					   sqlCmd.matches(".*\\sSELECT\\s.*") || sqlCmd.matches("SELECT\\s.*") ||
-					   sqlCmd.matches(".*\\sDELETE\\s.*") || sqlCmd.matches("DELETE\\s.*")) == false) 
+					   sqlCmd.matches(".*\\sDELETE\\s.*") || sqlCmd.matches("DELETE\\s.*")) == true) 
 						throw new IllegalArgumentException("SQL contains non select command, such as Insert, Select, Delete.");
 			
 			Class.forName("org.sqlite.JDBC");
@@ -143,7 +146,6 @@ public class DBCoordinator2 {
 			con.setAutoCommit(false);
 			System.out.println("Opened database successfully");
 
-			stmt = con.createStatement();
 			PreparedStatement prepStmt = con.prepareStatement(sqlCmd);
 			for (int i = 0; i < dataList.size(); i++) {
 				switch (typeList.get(i)) {
@@ -162,7 +164,6 @@ public class DBCoordinator2 {
 			prepStmt.execute();
 			con.commit();
 
-			stmt.close();
 			prepStmt.close();
 			con.close();
 		} catch (Exception e) {
