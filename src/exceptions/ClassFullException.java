91a1fe10f4b31b602fa91ec66abf7bb7a22bb5c2
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-12-01 03:07:43 -0600

    Commit ClassFullException and moved exception to build path folder.

---
+++ src/exceptions/ClassFullException.java
@@ -0,0 +1,38 @@
+/**
+ * @author wclee
+ *
+ */
+public class ClassFullException extends Exception {
+
+	/**
+	 * 
+	 */
+	public ClassFullException() {
+
+	}
+
+	/**
+	 * @param message
+	 */
+	public ClassFullException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ClassFullException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ClassFullException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+}
