ff91f6c6b6d017d89f4b5d5f1453e2747ccffe57
Yijia Zhang <yijia@Yijias-MacBook-Pro.local>
2015-12-03 14:20:09 -0600

    add course.java

---
+++ src/Course.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class Course {
+	private int courseID;
+	private String courseName;
+	private String institutionLocation;
+	private String instructorName;
+	private int credits;
+	private Date firstTime;
+	private Date lastTime;
+	private Time startTime;
+	private Time endTime;
+	private String classDays;
+	private String classType;
+	private String prereqs;
+	private String classDesc;
+	private String dept;
+
+
+};
+
