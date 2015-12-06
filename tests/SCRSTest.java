59d959c40667017c32575ffa52a5ae173c498783
yijia <zhan2398@umn.edu>
2015-12-06 14:18:15 -0600

    add tests

---
+++ tests/SCRSTest.java
@@ -0,0 +1,84 @@
+package edu.umn.csci5801;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SCRSTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testQueryClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryStudentPersonalData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryStudentRegistrationHistory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryAdminPersonalData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testQueryInstructor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStudentAddClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStudentDropClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStudentEditClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminAddClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminDeleteClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminEditClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminAddStudentToClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminEditStudentRegisteredClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdminDropStudentRegisteredClass() {
+		fail("Not yet implemented");
+	}
+
+}
