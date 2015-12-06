5c5f3396c59cd2bf069df64853f14eb421d956e1
yijia <zhan2398@umn.edu>
2015-12-06 14:25:46 -0600

    Change for course

--- src/edu/umn/csci5801/Course.java
+++ src/edu/umn/csci5801/Course.java
@@ -4,19 +4,22 @@ import java.util.Date;
 public class Course {
 	private int courseID;
 	private String courseName;
-	private String institutionLocation;
-	private String instructorName;
 	private int credits;
+	private int capacity;
+	private String term;
 	private Date firstTime;
 	private Date lastTime;
 	private String startTime;
 	private String endTime;
 	private String classDays;
+	private String institutionLocation;
 	private String classType;
 	private String prereqs;
 	private String classDesc;
 	private String dept;
+	
 
-
+	
 };
+
 
