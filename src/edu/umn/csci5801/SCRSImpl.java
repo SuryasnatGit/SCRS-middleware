fc8fa2ec88ff3dea3d382c6cae10305ea4ca86bb
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-12-06 21:02:24 -0600

    Update SCRSImpl based on new information (no need to handle auth)
    Also added all methods.

--- src/edu/umn/csci5801/SCRSImpl.java
+++ src/edu/umn/csci5801/SCRSImpl.java
@@ -16,11 +16,15 @@ import edu.umn.csci5801.ShibbolethAuth.Token.RoleType;
  */
 public class SCRSImpl implements SCRS{
 
-	public ShibbolethAuth authenticate;
 	private final List<ArrayList<String>> _empty = new ArrayList<ArrayList<String>>();
+	private Student student;
+	private Administrator admin;
+	private Search search;
 	
 	public SCRSImpl() {
-		authenticate = new ShibbolethAuth();
+		student = new Student();
+		admin = new Administrator();
+		
 	}
 
 	@Override
@@ -38,8 +42,7 @@ public class SCRSImpl implements SCRS{
 
 	@Override
 	public List<ArrayList<String>> queryStudentPersonalData(Token token, int studentID) {
-		Student student = new Student();
-		if(authenticate.TokenAuth(token)){
+		if(token.type != RoleType.UNDEFINED){
 			//validate if a student is making this call and ensure they only query for their own history
 			// a student should not request anyone else's history 
 			if(token.type == RoleType.STUDENT){
@@ -54,8 +57,7 @@ public class SCRSImpl implements SCRS{
 
 	@Override
 	public List<ArrayList<String>> queryStudentRegistrationHistory(Token token, int studentID) {
-		Student student = new Student();
-		if(authenticate.TokenAuth(token)){
+		if(token.type != RoleType.UNDEFINED){
 			//validate if a student is making this call and ensure they only query for their own history
 			// a student should not request anyone else's history 
 			if(token.type == RoleType.STUDENT){
@@ -70,8 +72,7 @@ public class SCRSImpl implements SCRS{
 
 	@Override
 	public List<ArrayList<String>> queryAdminPersonalData(Token token) {
-		Administrator admin = new Administrator();
-		if(authenticate.tokenAuth(token) && token.type != RoleType.STUDENT)
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT)
 		{
 			return admin.queryAdminPersonalData(token);
 		}
@@ -80,8 +81,7 @@ public class SCRSImpl implements SCRS{
 
 	@Override
 	public List<ArrayList<String>> queryInstructor(Token token, int instructorID) {
-		Administrator admin = new Administrator();
-		if(authenticate.tokenAuth(token) && token.type != RoleType.STUDENT)
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT)
 		{
 			return admin.queryInstructor(instructorID);
 		}
@@ -91,8 +91,8 @@ public class SCRSImpl implements SCRS{
 	@Override
 	public boolean studentAddClass(Token token, int courseID, String grading, String courseTerm) {
 		Student student = new Student();
-		if(authenticate.TokenAuth(token)){
-			return student.studentAddClass(courseID, grading, courseTerm);
+		if(token.type != RoleType.UNDEFINED){
+			return student.studentAddClass(token, courseID, grading, courseTerm);
 		}
 		//authentication failed
 		return false;
@@ -100,13 +100,17 @@ public class SCRSImpl implements SCRS{
 
 	@Override
 	public boolean studentDropClass(Token token, int courseID) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED){
+			return student.studentDropClass(token, courseID);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean studentEditClass(Token token, int courseID, String grading, String courseTerm) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED){
+			return student.studentEditClass(token, courseID, grading, courseTerm);
+		}
 		return false;
 	}
 
@@ -114,13 +118,19 @@ public class SCRSImpl implements SCRS{
 	public boolean adminAddClass(Token token, int courseID, String courseName, int courseCredits, String instructor,
 			String firstDay, String lastDay, String classBeginTime, String classEndTime, String weekDays,
 			String location, String type, String prerequisite, String description, String department) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT){
+			return admin.adminAddClass(token, courseID, courseName, courseCredits,
+					instructor, firstDay, lastDay, classBeginTime, classEndTime, 
+					weekDays, location, type, prerequisite, description, department);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean adminDeleteClass(Token token, int courseID) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT){
+			return admin.adminDeleteClass(token, courseID);
+		}
 		return false;
 	}
 
@@ -128,26 +138,36 @@ public class SCRSImpl implements SCRS{
 	public boolean adminEditClass(Token token, int courseID, String courseName, int courseCredits, String instructor,
 			String firstDay, String lastDay, String classBeginTime, String classEndTime, String weekDays,
 			String location, String type, String prerequisite, String description, String department) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT){
+			return admin.adminEditClass(token, courseID, courseName, courseCredits, 
+										instructor, firstDay, lastDay, classBeginTime, 
+										classEndTime, weekDays, location, type, prerequisite, description, department);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean adminAddStudentToClass(Token token, int studentID, int courseID, String grading, String courseTerm) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT){
+			return admin.adminAddStudentToClass(token, studentID, courseID, grading, courseTerm);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean adminEditStudentRegisteredClass(Token token, int studentID, int courseID, String grading,
 			String courseTerm) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT){
+			return admin.adminEditStudentRegisteredClass(token, studentID, courseID, grading, courseTerm);
+		}
 		return false;
 	}
 
 	@Override
 	public boolean adminDropStudentRegisteredClass(Token token, int studentID, int courseID) {
-		// TODO Auto-generated method stub
+		if(token.type != RoleType.UNDEFINED && token.type != RoleType.STUDENT){
+			return admin.adminDropStudentRegisteredClass(token, studentID, courseID);
+		}
 		return false;
 	}
 	
