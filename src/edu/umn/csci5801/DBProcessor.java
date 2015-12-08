6a2b7f8f47c27e567e92de3a0f1c2549b8431e97
Wen Chuan Lee <leewenchuan.lwc@gmail.com>
2015-12-07 21:16:02 -0600

    Added DBProcessor methods, able to type cast Longs as Dates now.

--- src/edu/umn/csci5801/DBProcessor.java
+++ src/edu/umn/csci5801/DBProcessor.java
@@ -1,8 +1,8 @@
 package edu.umn.csci5801;
 import java.util.List;
+
 import java.sql.Date;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -21,8 +21,7 @@ public final class DBProcessor {
 	/*
 	 * Function that down casts return types into strings
 	 */
-	public static ArrayList<ArrayList<String>> toStringArray(List<ArrayList<Object>> ObjectLists)
-	{
+	public static ArrayList<ArrayList<String>> toStringArray(List<ArrayList<Object>> ObjectLists) {
 		ArrayList<ArrayList<String>> listOfStrList = new ArrayList<ArrayList<String>>();
 		ArrayList<String> strList = new ArrayList<String>();
 		for(ArrayList<Object> Objects : ObjectLists)
@@ -36,8 +35,8 @@ public final class DBProcessor {
         			strList.add( (String) fields  );
         		else if(fields instanceof Integer)
         			strList.add( (String) fields.toString());
-        		else if(fields instanceof Date)
-        			strList.add( (new SimpleDateFormat("MM/dd/yyyy")).format(fields)  );
+        		else if(fields instanceof Long)
+        			strList.add( new Date((long) fields).toString());
           		else 
         		{
         			throw new ClassCastException("Could not cast: " + fields );
@@ -49,8 +48,31 @@ public final class DBProcessor {
         return listOfStrList;
 	}
 	
-	public static ArrayList<ArrayList<String>> runQuery(String sqlCmd) throws SQLException, ClassNotFoundException
-	{
+	public static int getIntegerFromQuery(String sqlCmd) throws SQLException, ClassNotFoundException {
+		DBCoordinator db = new DBCoordinator();
+		List<ArrayList<Object>> result = db.queryData(sqlCmd);
+		if(result.size() != 1)
+			throw new SQLException("Query returned more than 1 Integer.");
+		return (int) result.get(0).get(0);
+	}
+	
+	public static String getStringFromQuery(String sqlCmd) throws SQLException, ClassNotFoundException {
+		DBCoordinator db = new DBCoordinator();
+		List<ArrayList<Object>> result = db.queryData(sqlCmd);
+		if(result.size() != 1)
+			throw new SQLException("Query returned more than 1 String.");
+		return (String) result.get(0).get(0).toString();
+	}
+	
+	public static String getDateFromQuery(String sqlCmd) throws SQLException, ClassNotFoundException {
+		DBCoordinator db = new DBCoordinator();
+		List<ArrayList<Object>> result = db.queryData(sqlCmd);
+		if(result.size() != 1)
+			throw new SQLException("Query returned more than 1 Date.");
+		return new Date((long) result.get(0).get(0)).toString();
+	}
+	
+	public static ArrayList<ArrayList<String>> runQuery(String sqlCmd) throws SQLException, ClassNotFoundException {
 		DBCoordinator db = new DBCoordinator();
 		List<ArrayList<Object>> result = db.queryData(sqlCmd);
 		return toStringArray(result);
