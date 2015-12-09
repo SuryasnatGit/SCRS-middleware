7976d4b7925eb669e0daaa71758b1e64a6f0d509
kurt <kurt@192.168.0.130>
2015-12-08 19:00:06 -0800

    updated testDriver

--- tests/SimpleTestDriver.java
+++ tests/SimpleTestDriver.java
@@ -12,15 +12,35 @@ public class SimpleTestDriver {
 
 	Student something = new Student();
 	
+	Administrator administrator = new Administrator();
+	
+	ShibbolethAuth.Token token = null;
+	
 //	System.out.println(something.queryRegistrationHistory(0));
 	
 //	System.out.println(something.queryStudentPersonalData(5));
 	
-	System.out.println(something.studentAddClass(0, 2, "S/N","Fall2015"));
+	//System.out.println(something.studentAddClass(0, 2, "S/N","Fall2015"));
 	
 	//System.out.println(something.studentEditClass(0, 0, "AUD", "Fall2015"));
 	
-	System.out.println(something.studentDropClass(0,2));
+//	System.out.println(something.studentDropClass(0,2));
+	
+ //   System.out.println(administrator.adminEditClass(token, 2, "KurtClass", 3, "", "","", "", "" ,"", "", "","", "do stuff", ""));	
+	
+//	System.out.println(administrator.adminEditStudentRegisteredClass(token, 2, 2, "A-F", "Fall2015"));
+	
+	//	System.out.println(administrator.adminAddStudentToClass(token, 0, 2, "AUD", "Fall2015"));
+		
+	//	System.out.println(administrator.adminDropStudentRegisteredClass( token, 0, 2));
+		//just need to change courseid to do more adds change 2nd value 
+		
+//	    System.out.println(administrator.adminAddClass( token, 34, "compStuff", 4, 25, "Fall2015", 11, "1441947600000",
+	//			 "1452492000000", "10:01 am", "11:02 am", "Tu, Th", "East Campus", "Lecture",
+	//				"algorith", "none", "CS"));
+		
+	//	System.out.println(administrator.adminDeleteClass(token, 34));
+		
+	//	System.out.println(administrator.queryAdminPersonalData(token));
 	}
-
 }
